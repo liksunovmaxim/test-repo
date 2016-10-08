940f359fb573b4636c6d145ee5e5e7486b9bba55
alisitsa <andreylisitsa1982@gmail.com>
2016-10-08 15:39:35 +0300

    lesson 4 update

---
+++ frontend/javascripts/index.js
@@ -0,0 +1,8 @@
+$(window).on('load', function () {
+    $('.swiper-container').swiper({
+        arrows: true
+    });
+    console.log(6);
+});
+
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 'use strict';
 
-var gulp         = require('gulp'),
-    sass         = require('gulp-sass'),
-    sourcemaps   = require('gulp-sourcemaps'),
-    browserSync  = require('browser-sync').create(),
-    imagemin     = require('gulp-imagemin'),
-    watch        = require('gulp-watch'),
-    autoprefixer = require('gulp-autoprefixer');
+var gulp           = require('gulp'),
+    sass           = require('gulp-sass'),
+    sourcemaps     = require('gulp-sourcemaps'),
+    browserSync    = require('browser-sync').create(),
+    imagemin       = require('gulp-imagemin'),
+    watch          = require('gulp-watch'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    gulpFilter     = require('gulp-filter'),
+    flatten        = require('gulp-flatten'),
+    uglify         = require('gulp-uglifyjs'),
+    minifyjs       = require('gulp-js-minify'),
+    mainBowerFiles = require('gulp-main-bower-files');
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -16,6 +21,7 @@ gulp.task('browser-sync', function() {
     });
     gulp.watch('./frontend/stylesheets/**/*.scss', ['sass']);
     gulp.watch("./*.html").on('change', browserSync.reload);
+    gulp.watch("./public/**/*.js").on('change', browserSync.reload);
 });
 
 gulp.task('sass', function () {
@@ -30,7 +36,7 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('./public/css'));
 });
 
-gulp.task('image:build', function(){
+gulp.task('image:watch', ['image:build'], function(){
     return watch(['./frontend/images/**/*.jpg', './frontend/images/**/*.png', './frontend/images/**/*.svg', './frontend/images/**/*.gif'], function () {
         gulp.src(['./frontend/images/**/*.jpg', './frontend/images/**/*.png', './frontend/images/**/*.svg', './frontend/images/**/*.gif'])
             .pipe(imagemin())
@@ -38,4 +44,39 @@ gulp.task('image:build', function(){
     });
 });
 
-gulp.task('default', ['browser-sync', 'sass', 'image:build']);
+gulp.task('image:build', function(){
+    gulp.src(['./frontend/images/**/*.jpg', './frontend/images/**/*.png', './frontend/images/**/*.svg', './frontend/images/**/*.gif'])
+        .pipe(imagemin())
+        .pipe(gulp.dest('./public/images'));
+});
+
+gulp.task('javascripts:watch', ['javascripts:build'], function(){
+    return watch('./frontend/javascripts/**/*.js', function () {
+        gulp.src('./frontend/javascripts/**/*.js')
+            .pipe(uglify())
+            .pipe(gulp.dest('./public/js'));
+    });
+});
+
+gulp.task('javascripts:build', function(){
+    gulp.src('./frontend/javascripts/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('main-bower-files', function() {
+    var filterJS = gulpFilter('**/*.js', { restore: true });
+    return gulp.src('./bower.json')
+        .pipe(mainBowerFiles( ))
+        .pipe(filterJS)
+        .pipe(flatten())
+        .pipe(gulp.dest('./public/js'));
+});
+
+gulp.task('default', [
+    'browser-sync',
+    'sass',
+    'image:watch',
+    'main-bower-files',
+    'javascripts:watch'
+]);
